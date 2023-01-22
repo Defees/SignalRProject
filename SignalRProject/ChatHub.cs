c021149ad81e4ac1edc313bb284862b644f26757
Ivan_Bartov <vanya.bartov@gmail.com>
2023-01-22 16:59:56 +0200

    Add project files.

---
+++ SignalRProject/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+ 
+namespace SignalRProject
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(string message)
+        {
+            await this.Clients.All.SendAsync("Receive", message);
+        }
+    }
+}
