8fd0ccb6e184a14b99ad56ecdb01d93372d4cf55
Ivan_Bartov <vanya.bartov@gmail.com>
2023-01-22 17:15:45 +0200

    added few more features

--- SignalRProject/ChatHub.cs
+++ SignalRProject/ChatHub.cs
@@ -4,9 +4,9 @@ namespace SignalRProject
 {
     public class ChatHub : Hub
     {
-        public async Task Send(string message)
+        public async Task Send(string message, string userName)
         {
-            await this.Clients.All.SendAsync("Receive", message);
+            await this.Clients.All.SendAsync("Receive", message, userName);
         }
     }
 }
